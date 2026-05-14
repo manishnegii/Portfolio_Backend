59ff12d83f38c44b2c379467876fd24d83a10538
Manish <zerry808@gmail.com>
2026-05-14 22:08:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ app.use(cors());
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
-  service: "smtp.gmail.com",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure:false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
